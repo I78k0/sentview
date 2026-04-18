3e9f7e2deb389ca0f42bd8bbc299307cd959b2c0
Husam <husamalkuraimi8@gmail.com>
2026-04-19 01:48:32 +0300

    configure django to use postgresql

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sentview_db',
+        'USER': 'postgres',
+        'PASSWORD': 'emad1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
